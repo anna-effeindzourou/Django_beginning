83d738dd160cae348280bfdec7669ed3a33898e0
anna effeindzourou <anna.effeindzourou@gmail.com>
2017-07-01 23:05:18 +1000

    django beginnings

---
+++ rest-framework-tutorial/myenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/anna/Ilovemyhair_project/Django_beginning/rest-framework-tutorial/myenv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
